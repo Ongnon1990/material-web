74011e33c349e4ff3106495c439ec812cb33588b
e111077 <5981958+e111077@users.noreply.github.com>
2023-05-26 00:21:51 +0000

    Deploying to gh-pages from @ material-components/material-web@8c56abd2353e1f867e88e54ef282d9dc1d776006 🚀

--- js/inline/theme.js
+++ js/inline/theme.js
@@ -1,1 +1,1 @@
-"use strict";(()=>{function l(o,t,a="material-theme"){let e=globalThis[a];e||(e=new CSSStyleSheet,globalThis[a]=e,o.adoptedStyleSheets.push(e)),e.replaceSync(t),localStorage.setItem("theme-string",t)}var n=localStorage.getItem("theme-string");n&&l(document,n);})();
+"use strict";(()=>{function l(n,a,t="material-theme"){let e=globalThis[t];e||(e=new CSSStyleSheet,globalThis[t]=e,n.adoptedStyleSheets.push(e)),e.replaceSync(a),localStorage.setItem(t,a)}var o=localStorage.getItem("material-theme");o&&l(document,o);})();
